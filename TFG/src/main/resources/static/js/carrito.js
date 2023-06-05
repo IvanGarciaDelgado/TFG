68ed6fdfeee0bdd46eb516203d6ee7fa0dd4813e
ivan <ivan.garcia120@educa.madrid.org>
2023-06-05 20:28:12 +0200

    actualizacion

---
+++ TFG/src/main/resources/static/js/carrito.js
@@ -0,0 +1,8 @@
+function actualizarPrecioTotal(input) {
+    var cantidad = parseFloat(input.value);
+    var precio = parseFloat(input.parentNode.previousElementSibling.querySelector('h5').textContent.replace(',', '.'));
+    var precioTotal = cantidad * precio;
+
+    input.parentNode.nextElementSibling.querySelector('h5').textContent = precioTotal.toFixed(2);
+}
+
